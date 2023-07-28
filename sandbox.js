755acfbdc8bbe3bb7c40e9e310c4571b983c92b7
Jonathan Viegut <viegutj@gmail.com>
2023-07-28 16:08:13 -0500

    followed video for lesson 2

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,9 @@
-var character = 'mario';
+var character = 'luigi';
 console.log(character);
 var inputs = document.querySelectorAll('input');
+console.log('inputs', inputs);
 inputs.forEach(function (input) {
-    console.log(input);
+    console.log('input', input);
 });
+// NOTE: nothing will run in browser, the browser doesn't know TypeScript (.ts)
+// NOTE: don't worry about variable declarations being repeated! The browser will only read the .js file.
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,14 @@
-const character = 'mario';
-
+const character = 'luigi';
 console.log(character);
 
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll('input')
+
+console.log('inputs', inputs);
 
 inputs.forEach(input => {
-  console.log(input);
-});
+  console.log('input', input);
+})
+
+// NOTE: nothing will run in browser, the browser doesn't know TypeScript (.ts)
+
+// NOTE: don't worry about variable declarations being repeated! The browser will only read the .js file.
